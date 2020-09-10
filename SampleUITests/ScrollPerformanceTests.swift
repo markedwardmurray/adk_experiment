401def918c41f41623c7ed7f86ee159737a85087
Jon Lazar <jon.lazar@gmail.com>
2020-09-10 16:33:12 -0400

    fix texture scroll performance test. add auto layout scroll performance test.

--- SampleUITests/ScrollPerformanceTests.swift
+++ SampleUITests/ScrollPerformanceTests.swift
@@ -13,7 +13,26 @@ class ScrollPerformanceTests: XCTestCase {
   func testTextureScrollPerformance() throws {
     let app = XCUIApplication()
     app.launch()
+    app.buttons["Texture"].tap()
     let table = app.tables.firstMatch
+
+    let measureOptions = XCTMeasureOptions()
+    measureOptions.invocationOptions = [.manuallyStop]
+
+    if #available(iOS 14.0, *) {
+      measure(metrics: [XCTOSSignpostMetric.scrollDecelerationMetric], options: measureOptions) {
+        table.swipeUp(velocity: .fast)
+        stopMeasuring()
+        table.swipeDown(velocity: .fast)
+      }
+    }
+  }
+
+  func testAutoLayoutScrollPerformance() throws {
+    let app = XCUIApplication()
+    app.launch()
+    app.buttons["Auto Layout"].tap()
+    let table = app.collectionViews.firstMatch
 
     let measureOptions = XCTMeasureOptions()
     measureOptions.invocationOptions = [.manuallyStop]
