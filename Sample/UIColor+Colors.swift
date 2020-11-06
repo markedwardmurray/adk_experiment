7f44f9cd021acbfe89cc467000fad7970f3829b0
Mark Murray <markedwardmurray@gmail.com>
2020-11-05 19:16:36 -0500

    add Colors.xcassets and static extension file

---
+++ Sample/UIColor+Colors.swift
@@ -0,0 +1,71 @@
+//
+//  UIColor+Colors.swift
+//  Sample
+//
+//  Created by Murray, Mark on 11/5/20.
+//  Copyright © 2020 The New York Times. All rights reserved.
+//
+
+import Foundation
+
+extension UIColor {
+  static var breakingRed: UIColor {
+    UIColor(named: "breakingRed")!
+  }
+
+  static var developingOrange: UIColor {
+    UIColor(named: "developingOrange")!
+  }
+
+  static var messagingBlue: UIColor {
+    UIColor(named: "messagingBlue")!
+  }
+
+  static var opinionGrey: UIColor {
+    UIColor(named: "opinionGrey")!
+  }
+
+  static var timesBlack: UIColor {
+    UIColor(named: "timesBlack")!
+  }
+
+  static var timesGray10: UIColor {
+    UIColor(named: "timesGray10")!
+  }
+
+  static var timesGray20: UIColor {
+    UIColor(named: "timesGray20")!
+  }
+
+  static var timesGray30: UIColor {
+    UIColor(named: "timesGray30")!
+  }
+
+  static var timesGray40: UIColor {
+    UIColor(named: "timesGray40")!
+  }
+
+  static var timesGray50: UIColor {
+    UIColor(named: "timesGray50")!
+  }
+
+  static var timesGray60: UIColor {
+    UIColor(named: "timesGray60")!
+  }
+
+  static var timesGray70: UIColor {
+    UIColor(named: "timesGray70")!
+  }
+
+  static var timesGray80: UIColor {
+    UIColor(named: "timesGray80")!
+  }
+
+  static var timesGray90: UIColor {
+    UIColor(named: "timesGray90")!
+  }
+
+  static var timesWhite: UIColor {
+    UIColor(named: "timesWhite")!
+  }
+}
